6a2bdf38a22452ba5e37a406a811705f273da2cf
Connor Markwell <markwell.connor@gmail.com>
2021-10-06 23:16:05 -0400

    #20: Add .gitignore, Hide (and change) protected environment variables, Update README commands

--- blind_voting_app/settings_shared.py
+++ blind_voting_app/settings_shared.py
@@ -19,14 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vggfz2m-6o^#0evc22t-zt7l+xdwi7n)-5brh2@ax7^#meyw+d'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
