2191cbb6c5f719a735804192ca61afe54aa1e29b
Connor Markwell <markwell.connor@gmail.com>
2021-10-19 02:18:52 -0400

    #30: Move test location, Use development settings where possible

--- blind_voting_app/wsgi.py
+++ blind_voting_app/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blind_voting_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blind_voting_app.settings_development')
 
 application = get_wsgi_application()
