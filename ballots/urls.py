00a8c84e82f9303209ae08076afa999f6d51732a
jlyoung813 <joseph40@umbc.edu>
2021-11-03 19:00:14 -0400

    Added view for ballot creation. In the process of moving ballot admin to its own view.

--- ballots/urls.py
+++ ballots/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from .views import AddBallotView
 from . import views
 
 app_name = 'ballots'
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:ballot_id>/', views.detail, name='detail'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('add/', AddBallotView.as_view(), name='add'),
 ]
