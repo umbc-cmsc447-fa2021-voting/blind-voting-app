575e4d57d845483410ec64f1ce4cf54c013d6bb8
jlyoung813 <joseph40@umbc.edu>
2021-11-04 16:08:48 -0400

    Added tests for forms and views.

--- ballots/tests.py
+++ ballots/tests.py
@@ -1,15 +1,17 @@
 import datetime
 from django.contrib.auth.models import User
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.urls import reverse
 from django.utils import timezone
 
 from .models import Ballot
 from users.models import Profile
+from .forms import AddBallotForm
 
 class BallotModelTests(TestCase):
     @classmethod
     def setUpTestData(cls):
-        User.objects.create(first_name='John', last_name='Smith')
+        User.objects.create(first_name='John', last_name='Smith', username = 'testuser', password='12345')
         profile = User.profile
         profile.ssn = "555-55-5555"
         profile.district = "BaltimoreCounty"
@@ -42,17 +44,52 @@ class BallotModelTests(TestCase):
         recent_ballot = Ballot(pub_date=time)
         self.assertIs(recent_ballot.was_published_recently(), True)
 
-    def test_districts_match(self):
-        """
-        users should only see ballots that match their district
-        therefore the district field of both models should be comparable
-        """
-        user = User.objects.get(id=1)
-        test_ballot = Ballot()
-        test_ballot.district="BaltimoreCounty"
-        self.assertEqual(test_ballot.district, user.profile.district)
-
     """
     might want to add a test to make sure the due date of the ballot is after
     the publish. Don't want a ballot to be past due as soon as it is created
     """
+
+class BallotFormTests(TestCase):
+    def test_ballot_form_title(self):
+        form = AddBallotForm()
+        self.assertTrue(form.fields['ballot_title'].label is None or form.fields['ballot_title'].
+                        label == 'Ballot title')
+
+    def test_ballot_form_description(self):
+        form = AddBallotForm()
+        self.assertTrue(form.fields['ballot_description'].label is None or form.fields['ballot_description'].
+                        label == 'Ballot description')
+
+    def test_ballot_form_district(self):
+        form = AddBallotForm()
+        self.assertTrue(form.fields['ballot_description'].label is None or form.fields['district'].
+                        label == 'District')
+
+    def test_ballot_form_pub_date(self):
+        form = AddBallotForm()
+        self.assertTrue(form.fields['pub_date'].label is None or form.fields['pub_date'].
+                        label == 'Date published')
+
+    def test_ballot_form_description(self):
+        form = AddBallotForm()
+        self.assertTrue(form.fields['due_date'].label is None or form.fields['due_date'].
+                        label == 'Due date')
+
+class BallotAdminTests(TestCase):
+    def test_admin_url_exists(self):
+        response = self.client.get('/ballot-admin')
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_url_reverse(self):
+        response = self.client.get(reverse('ballots:ballot-admin'))
+        self.assertEqual(response.status_code, 200)
+
+class BallotAddTests(TestCase):
+    def test_add_url_exists(self):
+        response = self.client.get('/add/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_url_reverse(self):
+        response = self.client.get(reverse('ballots:add'))
+        self.assertEqual(response.status_code, 200)
+
