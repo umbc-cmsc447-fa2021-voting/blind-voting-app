1d4c6cd72e1a4808544f6474392e084bf2844b8a
PaulB52 <paulb1@umbc.edu>
2021-11-17 21:18:42 -0500

    #4: Most text fields can be edited. The fields that cannot be changed are the votes count field, the ballot a question is linked to and the choices that are with that question. We do not want the admins to change the vote total. We also don't want them to swap questions and choices around.

--- ballots/admin.py
+++ ballots/admin.py
@@ -11,6 +11,20 @@ class BallotsAdmin(admin.ModelAdmin):
     fieldsets = [(None, {'fields': ['ballot_title', 'ballot_description']}),
                  ('Date Information', {'fields': ['pub_date', 'due_date']}), ]
 
-admin.site.register(Choice)
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ["ballot"]  # Once the ballot is chosen it cannot be change
+        else:
+            return []
+
+class ChoiceAdmin(admin.ModelAdmin):
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ["question", "votes"]  # Once the question is chosen it cannot change
+        else:
+            return ["votes"]  # Makes the vote field readonly. The admins can not edit it
+
+admin.site.register(Choice, ChoiceAdmin)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Ballot, BallotsAdmin)
