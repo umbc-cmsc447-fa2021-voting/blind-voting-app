b1df554b70bd77750262fb47dd2249c26e7b5633
Connor Markwell <markwell.connor@gmail.com>
2021-12-07 11:50:09 -0500

    #47: Fix settings configuration

--- blind_voting_app/settings_shared.py
+++ blind_voting_app/settings_shared.py
@@ -108,7 +108,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-django_heroku.settings(local())
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
