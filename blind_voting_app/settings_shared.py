c9a8aeff388914b25b59c9e28317c9664724a416
Connor Markwell <markwell.connor@gmail.com>
2021-12-08 16:21:24 -0500

    #47: Manual email configuration using env variables

--- blind_voting_app/settings_shared.py
+++ blind_voting_app/settings_shared.py
@@ -122,17 +122,34 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 if os.getenv('DJANGO_ENV') != 'production':
     from .settings_development import *
 else:
+    # DB config
+
     DATABASES = { 'default': dj_database_url.config() }
 
     DATABASES['default']['ATOMIC_REQUESTS'] = True
 
     DEBUG = True
 
+    # For profile encryption
+
     DEFAULT_DOMAIN = 'blind-voting-app.herokuapp.com'
+
+    # Configure email
+
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+    EMAIL_HOST = os.getenv('EMAIL_HOST')
+
+    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+
+    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+    DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
     EMAIL_USE_TLS = True
 
     EMAIL_HOST_PORT = 587
+
 
 
 
